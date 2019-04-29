e4bf992d3086f36a8cdf70a17c6039a56071b7d7
Grant <grantpaulson6@gmail.com>
2019-04-29 09:38:26 -0700

    Complete configuration and env setup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery-rails
+//= require jquery_ujs
 //= require rails-ujs
 //= require activestorage
 //= require_tree .
---
+++ frontend/entry.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const root = document.getElementById('root');
+
+    ReactDOM.render(<h1>React is here</h1>, root);
+})
