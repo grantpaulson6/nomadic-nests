5554710a88976e2f72d8112c576f8fa212de3ffe
Grant <grantpaulson6@gmail.com>
2019-05-02 07:50:29 -0700

    update logo

--- frontend/components/nav_bar.jsx
+++ frontend/components/nav_bar.jsx
@@ -9,7 +9,8 @@ class NavBar extends React.Component {
         const left_nav = (
             <div className="left-nav">
                 <div className="logo">
-                    <i class="fas fa-fighter-jet"></i>
+                    {/* <div>Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div> */}
+                    <div className="main-logo"></div>
                 </div>
             </div>
         )
